6394710b5ec3fdc24ee1763344c0c261a0c0054a
BianchiJess <bianchi.jessica31@gmail.com>
2026-05-30 16:27:43 -0300

    Zelda

---
+++ Zeldinha/Assets/Scripts/Grass.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    public ParticleSystem fxGrass;
+    private bool isCut = false;
+
+    public void GetHit()
+    {
+        if (!isCut)
+        {
+            isCut = true;
+            fxGrass.Emit(Random.Range(10, 20));
+            transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        }
+    }
+}
